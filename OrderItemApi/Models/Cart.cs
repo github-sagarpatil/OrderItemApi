da008d1ab362b44c17bbf8e1022805a68e2da686
sagar <sagarcpatilscp@gmail.com>
2022-06-27 10:16:36 +0530

    Second commit

--- OrderItemApi/Models/Cart.cs
+++ OrderItemApi/Models/Cart.cs
@@ -12,11 +12,8 @@ namespace OrderItemApi.Models
         [Key]
 
         public int Id { get; set; }
-
         public int UserId { get; set; }
         public int MenuItemId { get; set; }
-
-
-        public string Name { get; set; }
+        public string MenuItemName { get; set; }
     }
 }
